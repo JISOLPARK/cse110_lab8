499cd80777d47c1a142baa98be15741f6714735a
JISOLPARK <108747841+jip007@users.noreply.github.com>
2023-05-28 21:25:20 -0700

    lab8 complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Lab 8 - Starter
+# Lab 8 
+1. The automated tests would fit within a Github action this makes sure that my code is tested before pushing the code into the branch. 
+2. no
+3. No, this is because the each part of the message feature depends on one another and is not one action. 
+4. Yes, the max message length should use unit test since it doesn't depend on other features and is only one action.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,64 @@
 // unit.test.js
-
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('phone number test 1', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+  });
+test('phone number test 2', () => {
+expect(functions.isPhoneNumber('(123)-456-7890')).toBe(true);
+});
+test('phone number test 3', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+test('phone number test 4', () => {
+    expect(functions.isPhoneNumber('12345678901')).toBe(false);
+});
+test('emaiil test 1', () => {
+    expect(functions.isEmail('hello@email.com')).toBe(true);
+});
+test('emaiil test 2', () => {
+    expect(functions.isEmail('test@gmail.com')).toBe(true);
+});
+test('emaiil test 3', () => {
+    expect(functions.isEmail('testgmail.com')).toBe(false);
+});
+test('emaiil test 4', () => {
+    expect(functions.isEmail('testgmailcom')).toBe(false);
+});
+test('password test 1', () => {
+    expect(functions.isStrongPassword('aB123456789')).toBe(true);
+});
+test('password test 2', () => {
+    expect(functions.isStrongPassword('SKka_bce1234')).toBe(true);
+});
+test('password test 3', () => {
+    expect(functions.isStrongPassword('')).toBe(false);
+});
+test('password test 4', () => {
+    expect(functions.isStrongPassword('12345')).toBe(false);
+});
+test('date test 1', () => {
+    expect(functions.isDate('1/31/2002')).toBe(true);
+});
+test('date test 2', () => {
+    expect(functions.isDate('01/31/2002')).toBe(true);
+});
+test('date test 3', () => {
+    expect(functions.isDate('01-31-2002')).toBe(false);
+});
+test('date test 4', () => {
+    expect(functions.isDate('01312002')).toBe(false);
+});
+test('hexCode test 1', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+test('hexCode test 2', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+test('hexCode test 3', () => {
+    expect(functions.isHexColor('')).toBe(false);
+});
+test('hexCode test 4', () => {
+    expect(functions.isHexColor('#yellow')).toBe(false);
+});
